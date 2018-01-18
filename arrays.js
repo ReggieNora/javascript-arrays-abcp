0252e13191fcad300c54acd0d208651f6116d4be
Reggie Nora <speedracr35@gmail.com>
2018-01-18 07:49:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,12 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+
+
+
+destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshft(element)
+  return array
+}
